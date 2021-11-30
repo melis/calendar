862a723e69ec8295dd32ee9afb592625b56ad259
Melis <59087686+melis@users.noreply.github.com>
2021-12-01 01:16:10 +0600

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Example from "./Example";
-// import "./index.css";
-import MyComponent from "./MyComponent";
 
-ReactDOM.render(<Example />, document.getElementById("root"));
+ReactDOM.render(<div>d</div>, document.getElementById("root"));
