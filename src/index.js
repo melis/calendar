d50512d144ce4d8c03251a52f0d10f67bf2c96a5
Melis <59087686+melis@users.noreply.github.com>
2021-12-26 13:13:02 +0600

    12

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 // import App from "./components/App/App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Exchange from "./components/Exchange/Exchange";
+// import Exchange from "./components/Exchange/Exchange";
+import Cansel from "./components/Cansel/Cansel";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Exchange />
+    <Cansel />
   </BrowserRouter>,
   document.getElementById("root")
 );
